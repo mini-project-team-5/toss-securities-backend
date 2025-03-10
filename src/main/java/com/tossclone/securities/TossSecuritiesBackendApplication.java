f7d163949b954467093360a584c2da3e482a52ab
kimminseo <kimminseo@DESKTOP-SLI222R>
2025-03-10 17:45:48 +0900

    main

---
+++ src/main/java/com/tossclone/securities/TossSecuritiesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tossclone.securities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TossSecuritiesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TossSecuritiesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tossclone/securities/TossSecuritiesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tossclone.securities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TossSecuritiesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
