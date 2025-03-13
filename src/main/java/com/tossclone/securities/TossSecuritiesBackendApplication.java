20f975178a2b9becda168b503724323f5151ed19
nue-os <kimsoeun0108@gmail.com>
2025-03-13 17:27:17 +0900

    chore: 프로젝트 기본 세팅

--- src/main/java/com/tossclone/securities/TossSecuritiesBackendApplication.java
+++ src/main/java/com/tossclone/securities/TossSecuritiesBackendApplication.java
@@ -2,8 +2,10 @@ package com.tossclone.securities;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:config/secu.properties")
 public class TossSecuritiesBackendApplication {
 
 	public static void main(String[] args) {
